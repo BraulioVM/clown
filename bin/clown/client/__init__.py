b1857f04bb9a9e0f51cab972c176e8f1721446c1
Braulio Valdivielso Martínez <brlvldvlsmrtnz@gmail.com>
2015-03-25 11:45:59 +0100

    Added logging capabilities to clown's client

--- bin/clown/client/__init__.py
+++ bin/clown/client/__init__.py
@@ -1,8 +1,14 @@
-import urllib2, os
+import urllib2, os, logging
 
 def clown():
 	clownd_url = "http://localhost:31415/api/repository"
-	repo_url = urllib2.urlopen(clownd_url).read()
+	try:
+		repo_url = urllib2.urlopen(clownd_url).read()
+		if repo_url != "[NO REPO]":
+			os.system("git clone " + repo_url)
+		else:
+			logging.warning("No repo was clowned. May the chrome extension not be installed?")
 
-	if repo_url != "[NO REPO]":
-		os.system("git clone " + repo_url)
+	except Exception as e:
+		logging.critical("clownd server seems not to be running")
+		logging.info("Execute clownd command to run the server (more info: <future link>)")
