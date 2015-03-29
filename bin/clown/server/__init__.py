77849fa62e552be3f74b9c4c0b47d85d972c5d78
Braulio Valdivielso Martínez <brlvldvlsmrtnz@gmail.com>
2015-03-29 18:59:01 +0200

    Avoid code injection

--- bin/clown/server/__init__.py
+++ bin/clown/server/__init__.py
@@ -3,12 +3,19 @@ app = Flask("Clownd server")
 
 repository = "[NO REPO]"
 
+def url_is_safe(url):
+	return url.find(";") == -1 and url.find("&") == -1
+
+
 @app.route("/api/repository", methods=["GET", "POST"])
 def repository_route():
 	global repository
 	
 	if request.method == "POST":
-		repository = request.form["repository"]
+		repository_url = request.form["repository"]
+		
+		if url_is_safe(repository_url):	# Avoid code injection
+			repository = repository_url
 
 
 	return repository
